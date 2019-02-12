b31afa51ce69e402a725bc9b65f3f106f247aa30
Салтанат Алимбекова <@local.itstep>
2019-02-12 17:47:14 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        #region
+        static string searchResult = @"[{'PIN':'10021 01','BRAND':'LMI','NAME':'опора амортизатора переднего!\\ Audi 100/A6 2.0-4.2 91-97','ARTID':'36680','PARNR':'0000109760',
+'KEYZAK':'0000006819','RVALUE':'1','RDPRF':'1','MINBM':'1.000','VENSL':'86.0','PRICE':'1691.00','WAERS':'RUB','DLVDT':'20170525181500','ANALOG':'','MSG':null,'PriceTenge':0,'PriceRub':0.0,
+'ArtificialPrice':null,'ShowArtificialByDefault':false,'SupplierId':0,'Delivery':null,'DeliveryEx':0,'DeliveryGu':0,'Vkorg':null,'Kunnr':null}, {'PIN':'10021 01','BRAND':'LMI','NAME':'опора амортизатора переднего!\\ Audi 100/A6 2.0-4.2 91-97','ARTID':'36680','PARNR':'0000139657','KEYZAK':'0000011450','RVALUE':'37','RDPRF':'1','MINBM':
+'1.000','VENSL':'54.0','PRICE':'555.00','WAERS':'RUB','DLVDT':'20170601181500','ANALOG':'','MSG':null,'PriceTenge':0,'PriceRub':0.0,'ArtificialPrice':null,'ShowArtificialByDefault':false,
+'SupplierId':0,'Delivery':null,'DeliveryEx':0,'DeliveryGu':0,'Vkorg':null,'Kunnr':null}";
+        #endregion
+        static void Main(string[] args)
+        {
+            //string[] resultArr =
+            //    searchResult
+            //    .Trim('[')
+            //    .Trim(']')
+            //    .Trim('{')
+            //    .Trim('}')
+            //    .Split(new string[] { "}, {" },
+            //            StringSplitOptions.None);
+
+            //for (int i = 0; i < resultArr.Length; i++)
+            //{
+            //    string[] res = resultArr[i].Split(',');
+            //    for (int g = 0; g < res.Length; g++)
+            //    {
+            //        string[] prop = res[g].Replace("'", "")
+            //            .Split(':');
+
+            //        if (prop[0] == "PIN" || prop[0] == "NAME" || prop[0] == "PRICE")
+            //        {
+            //            Console.WriteLine("{0}: {1}", prop[0], prop[1]);
+            //        }
+            //    }
+            //        Console.WriteLine("--------------------------------------------------------");
+
+
+            //}
+
+            string N = "1234567891011121314151617181955";
+
+            int sum = 0;
+            for (int i = 0; i < N.Length; i++)
+            {
+                Console.WriteLine(N[i]);
+                sum += Int32.Parse(N[i].ToString());
+            }
+            Console.WriteLine(sum);
+
+
+
+        }
+
+       
+    }
+}
---
+++ string/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            string str0 = new string('*', 20);
+
+            string str1 = "г.Алматы";
+            string str2 = "г.Алматы";
+            if(str==str2)
+            { }
+            if(str1.Equals(str2))
+            { }
+            if(string.Compare(str1, str2, true)==0)
+            { }
+
+            Console.WriteLine(str1.CompareTo(str2));
+
+            //concat
+
+            string result = "Адрес: ";
+            result = result + str1;
+
+            string.Concat(result, str1, "-", str2);
+
+            //contains
+
+            Console.WriteLine(result.Contains("Алматы"));
+
+            //StartWith
+            string card = "4003**0327**7395**5051";
+            string[] cardArr = card.Split(' ');
+
+            string[] cardArr2 = card.Split(new String[] {"**"}, StringSplitOptions.None);
+            //Join
+            string cardJoin = string.Join("-", cardArr2);
+
+            //trim
+
+
+            if (card.StartsWith("4003"))
+            {
+                Console.WriteLine("VISA");
+            }
+            string url = "https://mystat.itstep.org/en/main/homework/page/index";
+            int findPlacePosition = url.IndexOf("?", 0); // возвращает первое вхождение
+            int findPlacePosition2 = url.LastIndexOf("?", 0); // возвращает первое вхождение
+            Console.WriteLine();
+
+            string address = "{city Almaty}";
+            string trimAdderss = address.Trim('}');
+
+            address = address.Insert(1, "Адрес");
+
+            address = address.Remove(0, 1);
+
+            address = address.Replace("city", "город");
+
+            var test = address.Substring(0, 5);
+            var test1 = address.Substring(address.IndexOf("{")+1,5);
+
+            // фОРМАТИРОВАНИЕ СТРОК
+            DateTime cd = DateTime.Now;
+            string format1 = string.Format("{0:hh:mm} - {1:yyyy}", cd, cd);
+
+            StringBuilder sb = new StringBuilder("test str");
+            sb.Append()
+
+        }
+    }
+}
